2790d4c5f079436890f20acc4d2bed63ccb4031c
degentod <herufebrian2@gmail.com>
2026-07-19 20:55:09 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+self.lbl_page_indicator = QLabel("Page 1 / 1", self)
+self.lbl_page_indicator.setStyleSheet("font-weight: bold; color: #8E44AD; min-width: 60px; qproperty-alignment: AlignmentFlag.AlignCenter;")
+header_layout.addWidget(self.lbl_page_indicator)
